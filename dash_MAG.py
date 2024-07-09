513b47f76a61bc0e13c514b86ca1fa71225ffd58
aababc1 <hyeongwon.tg@gmail.com>
2024-07-09 14:56:36 +0900

    Initial commit of Dash app

--- dash_MAG.py
+++ dash_MAG.py
@@ -13,6 +13,21 @@ df = pd.read_csv('combined_metadata.csv')
 df_raw = pd.read_csv('combined_metadata.csv')
 df = df.fillna('NA')
 
+# reformat classificaiton  for readibility 
+def format_classification(text, break_after=3):
+    # Detect the delimiter by checking the occurrence
+    delimiter = ';' if ';' in text else '/'
+    
+    parts = text.split(delimiter)
+    if len(parts) > break_after:
+        # Insert a line break after the third part
+        parts[break_after] = "<br>" + parts[break_after]
+    return delimiter.join(parts)
+    
+df['formatted_classification'] = df['classification'].apply(format_classification)
+
+
+
 # Define taxonomic ranks
 tax_ranks = ['domain', 'phylum', 'class', 'order', 'family', 'genus', 'species']
 
@@ -21,7 +36,7 @@ df[tax_ranks] = df['classification'].str.split(';', expand=True)
 
 metadata_columns = [col for col in df.columns if col not in tax_ranks + ['classification', 'Genome']]
 
-dropdown_options = [{'label': col, 'value': col} for col in metadata_columns]
+dropdown_options = [{'label': col, 'value': col} for col in metadata_columns if col != 'formatted_classification']
 
 def create_sunburst(selected_metadata):
     path = tax_ranks + [selected_metadata]
@@ -29,6 +44,7 @@ def create_sunburst(selected_metadata):
         df, 
         path=path,
         color=selected_metadata,
+        
         color_continuous_scale=px.colors.sequential.RdBu,
         maxdepth=7  # This will show up to species level (6) plus one more for metadata
     )
@@ -89,11 +105,11 @@ app.layout = html.Div([
 
     html.Div([
         html.Div([
-            html.H2('Quality and number of genomes', style={'textAlign': 'left'}),
+            html.H2('Quality and number of all genomes', style={'textAlign': 'left'}),
             html.Label('Select your metadata', style={'fontSize': 15, 'fontWeight': 'bold', 'marginBottom': '10px'}),
             dcc.Dropdown(
                 id='color-dropdown',
-                options=[{'label': col, 'value': col} for col in metadata_columns],
+                options=[{'label': col, 'value': col} for col in metadata_columns if col != 'formatted_classification'],
                 value=metadata_columns[0] if metadata_columns else None,
                 placeholder="Select color variable",
                 style={
@@ -108,11 +124,11 @@ app.layout = html.Div([
 
         html.Div([
             dcc.Graph(id='overall-scatter-plot', style={'height': '600px'})
-        ], style={'width': '65%', 'display': 'inline-block'}),
+        ], style={'width': '65%', 'display': 'inline-block', 'overflow': 'visible'}),
         html.Div([
             dcc.Graph(id='overall-distribution-plot', style={'height':'600px'})
-        ], style={'width': '35%', 'display': 'inline-block', 'vertical-align': 'top'}),
-    ], style=common_style),    
+        ], style={'width': '35%', 'display': 'inline-block', 'vertical-align': 'top', 'overflow': 'visible'}),
+    ], style={**common_style, 'overflow': 'visible'}),    
 
     html.Div([
         html.Div([
@@ -132,16 +148,15 @@ app.layout = html.Div([
         html.Div([
             html.Div([
                 dcc.Graph(id='sunburst-plot')
-            ], style={'width': '65%', 'display': 'inline-block'}),
-            html.Div([
-                html.Div([
-                    dcc.Graph(id='filtered-scatter-plot', style={'height': '350px'})
-                ]),        
-                html.Div([
-                    dcc.Graph(id='filtered-distribution-plot', style={'height': '350px'})
-                ])
-            ], style={'width': '35%', 'display': 'inline-block', 'vertical-align': 'top'}),
-        ]),
+            ],  style={'display': 'flex', 'justify-content': 'center', 'align-items': 'center', 'height': '100%'}),
+            #style={'width': '80%', 'display': 'inline-block','overflow': 'visible'}),
+        ], style={'overflow': 'visible'}),
+
+        html.Div([
+            dcc.Graph(id='filtered-scatter-plot', style={'width': '65%', 'display': 'inline-block', 'height': '600px'}),
+            dcc.Graph(id='filtered-distribution-plot', style={'width': '35%', 'display': 'inline-block', 'height': '600px'})
+        ], style={'width': '100%', 'display': 'flex', 'overflow': 'visible'}),
+
 
         html.Div([
             dcc.Input(
@@ -162,7 +177,7 @@ app.layout = html.Div([
         html.Div([
             dcc.Dropdown(
                 id='column-selector',
-                options=[{'label': col, 'value': col} for col in df.columns],
+                options=[{'label': col, 'value': col} for col in df.columns  if col != 'formatted_classification'],
                 value=[col for col in df.columns[:10]],  # default selected columns
                 multi=True,
                 placeholder="Select columns to display"
@@ -171,7 +186,7 @@ app.layout = html.Div([
 
         dash_table.DataTable(
             id='data-table',
-            columns=[{"name": i, "id": i} for i in df.columns[:10]],  # Adjust number of columns displayed        
+            columns=[{"name": i, "id": i} for i in df.columns[:10] if i != 'formatted_classification'],  # Adjust number of columns displayed        
             data=df.head(10).to_dict('records'),
             page_size=10,
             style_table={'overflowX': 'scroll'},  # Enable horizontal scroll
@@ -196,7 +211,7 @@ def update_overall_plots(color_var):
         x='Completeness', 
         y='Contamination', 
         color=color_var,
-        hover_data=['classification'],
+        hover_data=['formatted_classification'],
         title='Overall: Completeness vs Contamination'
     )
     scatter_fig.update_layout(plot_bgcolor='white', font={'family': 'Arial', 'size': 14})
@@ -224,6 +239,9 @@ def update_sunburst(selected_metadata):
      Input('metadata-dropdown', 'value')],
     [State('sunburst-plot', 'figure')]
 )
+
+
+
 def update_filtered_plots(clickData, selected_metadata, current_figure):
     filtered_df = df.copy()
     
@@ -235,15 +253,18 @@ def update_filtered_plots(clickData, selected_metadata, current_figure):
     
     if filtered_df.empty:
         return dash.no_update, dash.no_update
+    
+    hover_data = ['formatted_classification'] + metadata_columns
 
     scatter_fig = px.scatter(
         filtered_df, 
         x='Completeness', 
         y='Contamination', 
         color=selected_metadata,
-        hover_data=['classification'],
+        hover_data=hover_data,
         title='Filtered: Completeness vs Contamination'
     )
+    
     scatter_fig.update_layout(plot_bgcolor='white', font={'family': 'Arial', 'size': 14})
 
     metadata_dist_fig = px.histogram(
@@ -281,7 +302,9 @@ def update_table(search_value, clickData, selected_metadata):
     Input('column-selector', 'value')
 )
 def update_table_columns(selected_columns):
-    return [{"name": i, "id": i} for i in selected_columns]
+    valid_columns = [col for col in selected_columns if col != 'formatted_classification']
+    return [{"name": col, "id": col} for col in valid_columns]
+    #return [{"name": i, "id": i} for i in selected_columns if i != 'formatted_classification']
 
 # download selected genome in the users local server callback
 @app.callback(
